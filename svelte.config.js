1bad17f1055dac8876f35fcc540491bd10269e61
joypunk82 <joypunk@pm.me>
2025-10-04 21:53:08 -0500

    Cleanup svelte adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,12 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
 	kit: {
-		// Static adapter configuration for GitHub Pages
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		}),
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/tentwentynine' : ''
-		}
+		adapter: adapter(),
+		// Remove base path for Vercel
 	}
 };
 
